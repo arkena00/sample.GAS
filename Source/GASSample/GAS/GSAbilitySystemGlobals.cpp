998e7d3300d655d6075f4796cfe0f58c460c3736
Arkena <ads.neuroshok@gmail.com>
2023-07-13 18:35:50 +0200

    [udpate]

---
+++ Source/GASSample/GAS/GSAbilitySystemGlobals.cpp
@@ -0,0 +1,12 @@
+﻿#include "GSAbilitySystemGlobals.h"
+
+FGSEffectContext* UGSAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+    return new FGSEffectContext;
+}
+
+// Add custom parameters to gameplay cues
+void UGSAbilitySystemGlobals::InitGameplayCueParameters(FGameplayCueParameters& CueParameters,
+    const FGameplayEffectContextHandle& EffectContext)
+{
+}
---
+++ Source/GASSample/GAS/GSEffectContext.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "GSEffectContext.generated.h"
+
+USTRUCT()
+struct FGSEffectContext : public FGameplayEffectContext
+{
+    GENERATED_BODY()
+
+    virtual UScriptStruct* GetScriptStruct() const override
+    {
+        return FGSEffectContext::StaticStruct();
+    }
+
+    virtual FGSEffectContext* Duplicate() const override
+    {
+        FGSEffectContext* NewContext = new FGSEffectContext();
+        *NewContext = *this;
+        // Copy context data here
+        NewContext->AddActors(Actors);
+
+        return NewContext;
+    }
+
+    virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override
+    {
+        bOutSuccess &= Super::NetSerialize(Ar, Map, bOutSuccess);
+
+        // Data serialization
+        return true;
+    }
+
+private:
+    // Custom data
+};
+
+template<>
+struct TStructOpsTypeTraits<FGSEffectContext> : public TStructOpsTypeTraitsBase2<FGSEffectContext>
+{
+    enum
+    {
+        WithNetSerializer = true,
+        WithCopy = true
+    };
+};
